2b0f2d70d0f2a6947fa2fe9acc850871f87293d0
Angel Vanegas <angelman@gmail.com>
2017-07-30 17:39:35 -0400

    create test function

--- pitch_test.rb
+++ pitch_test.rb
@@ -1,17 +1,27 @@
 require './lib/pitches'
 
+def set_add set, set2
+  (set | set2) - (set & set2)
+end
+
 f = Pitch.new('b', 4)
 d = Pitch.new('g#', 1)
 i = Interval.new(f, d)
 mi = MidiInterval.new(f, d)
 mc = MidiCalc.new(f)
 
-
 s = Scale.new(Pitch.new('c'), 'ionian')
+s1 = Scale.new(Pitch.new('f#'), 'dorian')
 p s.pc_scale
 p s.scale
 
-p i.ordered_pi
-p i.unordered_pi
-p i.ordered_pc_interval
-p i.interval_class
+p i.ordered_pi == -39
+p i.unordered_pi == 39
+p i.ordered_pc_interval == 9
+p i.interval_class == 3
+
+
+
+p s.pc_scale
+p s1.pc_scale
+p set_add(s.pc_scale, s1.pc_scale)
