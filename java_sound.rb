eae3db3ea3992f026e0dcc0355f72bb3b1054f2e
Angel Vanegas <angelman@gmail.com>
2017-08-02 21:30:21 -0400

    more stuff

--- java_sound.rb
+++ java_sound.rb
@@ -3,26 +3,37 @@ include Java
 MidiSystem = javax.sound.midi.MidiSystem
 ShortMsg = javax.sound.midi.ShortMessage
 
+@fluid = "FluidSynth virtual port (37884)"
+@gervill = "Gervill"
+
 def device
- MidiSystem.get_midi_device_info.select {|x| x.get_name.eql? "Gervill"}.first
+ MidiSystem.get_midi_device_info[1]#.select {|x| x.get_name.eql? @fluid}.first
 end
 
 dv = MidiSystem.get_midi_device(device)
 dv.open
-rec = dv.get_receiver
+@rec = dv.get_receiver
 
 #duration is needed, volume
+def duration d
+  f = rand
+  dur = rand + 0.05
+  if f > 0.3
+    d
+  else
+    dur
+  end
+end
 
 def play_notes arr, rec
+  d = 0.17
   arr.each do |note|
     toggle = ShortMsg.new
-    toggle.set_message(ShortMsg::NOTE_ON, 0, note, 100)
+    toggle.set_message(ShortMsg::NOTE_ON, 0, note, (rand(98)+25))
     rec.send(toggle, 0)
-    sleep (rand + 0.125)
+    sleep duration(d)
     toggle.set_message(ShortMsg::NOTE_OFF, 0, note, 100)
     rec.send(toggle, 0)
   end
   sleep 0.5
 end
-
-play_notes [81, 42, 83, 52, 75, 37, 68, 40, 59, 48, 62, 45, 65, 43], rec
--- pitch_test.rb
+++ pitch_test.rb
@@ -1,31 +1,47 @@
 require './lib/pitches'
+require './java_sound'
 
 def set_add(set, set2)
   (set | set2) - (set & set2)
 end
 
-f = Pitch.new('b', 4)
-d = Pitch.new('g#', 1)
-i = Interval.new(f, d)
-mi = MidiInterval.new(f, d)
-mc = MidiCalc.new(f)
+# f = Pitch.new('b', 4)
+# d = Pitch.new('g#', 1)
+# i = Interval.new(f, d)
+# mi = MidiInterval.new(f, d)
+# mc = MidiCalc.new(f)
 
-s = Scale.new(Pitch.new('c'), 'ionian')
+s = Scale.new(Pitch.new('c#'), 'ionian')
 s1 = Scale.new(Pitch.new('g'), 'phrygian')
-p s.pc_scale
-p s1.scale
+s2 = Scale.new(Pitch.new('f#'), 'dorian')
+s3 = Scale.new(Pitch.new('d'), 'locrian')
+s4 = Scale.new(Pitch.new('a'), 'aeolian')
+s5 = Scale.new(Pitch.new('b'), 'mixolydian')
 
-p i.ordered_pi == -39
-p i.unordered_pi == 39
-p i.ordered_pc_interval == 9
-p i.interval_class == 3
 
-p s.pc_scale
-p s1.pc_scale
+# p i.ordered_pi == -39
+# p i.unordered_pi == 39
+# p i.ordered_pc_interval == 9
+# p i.interval_class == 3
+
+# p s.scale
+# p s1.scale
 # p set_add(s.pc_scale, s1.pc_scale)
 
-# p s1.scale.map {|note|
-#   MidiCalc.new(Pitch.new(note, rand(4)+2)).pc_to_midi
-# }.shuffle + s.scale.map {|note|
-#   MidiCalc.new(Pitch.new(note, rand(4)+2)).pc_to_midi
-# }.shuffle
+def melody scales
+  oct = rand(4) + 2
+  scales.scale.map {|note|
+    MidiCalc.new(Pitch.new(note, oct)).pc_to_midi
+  }.shuffle
+end
+
+def repeat mel, num
+  arr = []
+  num.times do
+  arr += melody(mel)
+  end
+  arr
+end
+
+puts "Random melody..."
+play_notes(repeat(s1, 30), @rec)
